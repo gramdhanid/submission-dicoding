c6900050f87cb871541a342d3477a2b70ccc1788
Ramdhan <gilangramdhan.id@gmail.com>
2024-10-29 21:59:30 +0700

    add feature books

---
+++ src/server.js
@@ -0,0 +1,53 @@
+const Hapi = require('@hapi/hapi');
+const {
+  addBookHandler,
+  getAllBooksHandler,
+  getBookByIdHandler,
+  updateBookHandler,
+  deleteBookHandler,
+} = require('./handlers');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 9000,
+    host: 'localhost',
+  });
+
+  server.route([
+    {
+      method: 'POST',
+      path: '/books',
+      handler: addBookHandler,
+    },
+    {
+      method: 'GET',
+      path: '/books',
+      handler: getAllBooksHandler,
+    },
+    {
+      method: 'GET',
+      path: '/books/{bookId}',
+      handler: getBookByIdHandler,
+    },
+    {
+      method: 'PUT',
+      path: '/books/{bookId}',
+      handler: updateBookHandler,
+    },
+    {
+      method: 'DELETE',
+      path: '/books/{bookId}',
+      handler: deleteBookHandler,
+    },
+  ]);
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+process.on('unhandledRejection', (err) => {
+  console.log(err);
+  process.exit(1);
+});
+
+init();
